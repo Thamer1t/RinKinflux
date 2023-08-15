730ad14f3a575f8f6e6c10cd104df668520dadd8
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 12:23:17 +0300

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
-//conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+conn.sendFile(m.chat, ronaldo, 'error.jpg', `*سييييي*`, m)}
+conn.sendButton(m.chat, "*سييييييييييييي*", author, ronaldo, [['⚽ التالي ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['internet']
-handler.command = /^(ronaldo|cr7)$/i
+handler.command = /^(القوت|رونالدو)$/i
 export default handler
