6f288e9a925d51339243b70540a89c820e8c2ae2
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 15:05:47 +0300

    Update econ-todiamond.js

--- plugins/econ-todiamond.js
+++ plugins/econ-todiamond.js
@@ -1,21 +1,21 @@
 const xpperdiamond = 350 
 let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^todiamond/i, '')
+  let count = command.replace(/^الى-الماس/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperdiamond) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.db.data.users[m.sender].exp >= xpperdiamond * count) {
     global.db.data.users[m.sender].exp -= xpperdiamond * count
     global.db.data.users[m.sender].diamond += count
     conn.reply(m.chat, `
-┌─「 *EXCHANGED* 」
-‣ *Nominal ammount* : + ${count}💎 
-‣ *Used* : -${xpperdiamond * count} XP
+┌─「 *تم الاستبدال* 」
+‣ *الكمية* : + ${count}💎 
+‣ *المستعمل* : -${xpperdiamond * count} XP
 └──────────────`, m)
-  } else conn.reply(m.chat, `❎ you dont have enough xp for *${count}* Diamonds💎\n\nyou can get xp from games and economy*`, m)
+  } else conn.reply(m.chat, `❎ ماعندك اكس بي كافي لتحويله الى *${count}* الماس💎\n\nتقدر تجمع اكس بي من الأالعاب والاقتصاد*`, m)
 }
-handler.help = ['todiamond', 'todiamondall']
-handler.tags = ['econ']
-handler.command = ['todiamond', 'todiamndall'] 
+handler.help = ['الى-الماس', 'الى-الماس-كامل']
+handler.tags = ['اقتصاد']
+handler.command = ['الى-الماس', 'الى-الماس-كامل'] 
 
 handler.disabled = false
 
