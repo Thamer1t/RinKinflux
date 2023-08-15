c12dd530c0d6a8194ecd31b1d8e2026c4f12b4d7
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 12:53:46 +0300

    Update anime-rkill.js

--- plugins/anime-rkill.js
+++ plugins/anime-rkill.js
@@ -5,31 +5,31 @@ import MessageType from '@adiwajshing/baileys'
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-	
-	 let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @tag` 
-    
-    let user = global.db.data.users[who]
-    let name = conn.getName(who) 
-   let name2 = conn.getName(m.sender) 
-   m.react(rwait)
+
+  let who
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+  else who = m.chat
+  if (!who) throw `✳️ قم بالرد أو منشن شخص\n\n📌 مثال: ${usedPrefix + command} @منشن`
+
+  let user = global.db.data.users[who]
+  let name = conn.getName(who)
+  let name2 = conn.getName(m.sender)
+  m.react(rwait)
 
   let rki = await fetch(`https://api.waifu.pics/sfw/kill`)
-    if (!rki.ok) throw await rki.text()
-   let jkis = await rki.json()
-   let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) killed`, `${name}`)
-   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-   m.react('🗡️') 
-   
+  if (!rki.ok) throw await rki.text()
+  let jkis = await rki.json()
+  let { url } = jkis
+  let stiker = await sticker(null, url, `(${name2}) قتل`, `${name}`)
+  conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+  m.react('🗡️')
+
 }
 
-handler.help = ['kill @tag']
-handler.tags = ['rnime']
-handler.command = /^(kill|matar)$/i
-handler.diamond = true
+handler.help = ['قتل @منشن']
+handler.tags = ['رياكشن']
+handler.command = /^(قتل|matar)$/i
+handler.diamond = false
 handler.group = true
 
 export default handler
