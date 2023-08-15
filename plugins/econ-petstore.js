9c68ba0f09af5d1616661084789cb88ed3224a92
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 14:50:22 +0300

    Update econ-petstore.js

--- plugins/econ-petstore.js
+++ plugins/econ-petstore.js
@@ -1,10 +1,11 @@
+
 let handler = async (m, { conn, command, args }) => {
   let type = (args[0] || '').toLowerCase()
   let user = global.db.data.users[m.sender]
   let htki = '––––––『'
   let htka = '』––––––'
   
-  //----------HARGA
+  //----------الأسعار
   let hdog = 2
   let hcat = 2
   let hhorse = 4
@@ -13,19 +14,19 @@ let handler = async (m, { conn, command, args }) => {
 
   let caption = `
 🐈 • *قطة:* 
-➞ ${hcat} عملة حيوان أليف🔖
+➞ ${hcat} رمز الحيوان الأليف🔖
 🐕 • *كلب:*
-➞ ${hdog} عملة حيوان أليف🔖
+➞ ${hdog} رمز الحيوان الأليف🔖
 🐎 • *حصان:* 
-➞ ${hhorse} عملة حيوان أليف🔖
+➞ ${hhorse} رمز الحيوان الأليف🔖
 🦊 • *ثعلب:* 
-➞ ${hfox} عملة حيوان أليف🔖
-🍖 • *طعام حيوانات:*
+➞ ${hfox} رمز الحيوان الأليف🔖
+🍖 • *طعام الحيوان الأليف:*
 ➞ ${hpetfood} نقود 💹
 - - - - - - - - - - - - - - - - - - - - -
 ${htki} القدرة ${htka}
 ➞ 🐈 • قطة :
-- تزيد الصحة بنسبة 5% / لكل مستوى عند استخدام *.علاج*
+- زيادة الصحة 5% / المستوى عند استخدام *.heal*
 ➞ 🐕 • كلب :
 - قريبًا...
 ➞ 🐎 • حصان :
@@ -35,57 +36,75 @@ ${htki} القدرة ${htka}
 `
 
   try {
-    if (/petshop/i.test(command)) {
+    if (/متجر-الحيوانات/i.test(command)) {
       switch (type) {
-        case 'cat':
-          if (user.cat > 0) return m.reply('تملكها بالفعل!')
-          if (user.pet < hcat) return m.reply(`عملات الحيوان الأليف خاصتك غير كافية !`)
+        case 'قطة':
+          if (user.cat > 0) return m.reply('لديك هذه القطة بالفعل!')
+          if (user.pet < hcat) return m.reply(`رصيد عملة الحيوان الأليف لديك غير كافٍ!`)
           global.db.data.users[m.sender].pet -= hcat
           global.db.data.users[m.sender].cat += 1
-          conn.sendMessage(m.chat, { text: `*${htki} حيوان جديد !${htka}*\n\n🎉 لقد اشتريت *قطة*`, quoted: m })
+          conn.sendMessage(m.chat, { text: `*${htki} حيوان أليف جديد !${htka}*\n\n🎉 مبروك، لقد اشتريت حيوان أليف *قطة*`, quoted: m })
           break
-        case 'dog':
-          if (user.dog > 0) return m.reply('تملكه بالفعل!!')
-          if (user.pet < hdog) return m.reply(`عملات الحيوان الأليف خاصتك غير كافية !`)
+        case 'كلب':
+          if (user.dog > 0) return m.reply('لديك هذا الكلب بالفعل!')
+          if (user.pet < hdog) return m.reply(`رصيد عملة الحيوان الأليف لديك غير كافٍ!`)
           global.db.data.users[m.sender].pet -= hdog
           global.db.data.users[m.sender].dog += 1
-          conn.sendMessage(m.chat, { text: `*${htki} حيوان جديد !${htka}*\n\n🎉 لقد اشتريت *كلب*`, quoted: m })
+          conn.sendMessage(m.chat, { text: `*${htki} حيوان أليف جديد !${htka}*\n\n🎉 مبروك، لقد اشتريت حيوان أليف *كلب*`, quoted: m })
           break
-        case 'fox':
-          if (user.fox > 0) return m.reply('تملكه بالفعل!!')
-          if (user.pet < hfox) return m.reply(`عملات الحيوان الأليف خاصتك غير كافية !`)
+        case 'ثعلب':
+          if (user.fox > 0) return m.reply('لديك هذا الثعلب بالفعل!')
+          if (user.pet < hfox) return m.reply(`رصيد عملة الحيوان الأليف لديك غير كافٍ!`)
           global.db.data.users[m.sender].pet -= hfox
           global.db.data.users[m.sender].fox += 1
-          conn.sendMessage(m.chat, { text: `*${htki} حيوان جديد !${htka}*\n\n🎉 لقد اشتريت *ثعلب*`, quoted: m })
+          conn.sendMessage(m.chat, { text: `*${htki} حيوان أليف جديد !${htka}*\n\n🎉 مبروك، لقد اشتريت حيوان أليف *ثعلب*`, quoted: m })
           break
-        case 'horse':
-          if (user.horse > 0) return m.reply('تملكه بالفعل!')
-          if (user.pet < hhorse) return m.reply(`عملات الحيوان الأليف خاصتك غير كافية !`)
+        case 'حصان':
+          if (user.horse > 0) return m.reply('لديك هذا الحصان بالفعل!')
+          if (user.pet < hhorse) return m.reply(`رصيد عملة الحيوان الأليف لديك غير كافٍ!`)
           global.db.data.users[m.sender].pet -= hhorse
           global.db.data.users[m.sender].horse += 1
-          conn.sendMessage(m.chat, { text: `*${htki} حيوان جديد !${htka}*\n\n🎉 لقد اشتريت *حصان*`, quoted: m })
-          break
-        case 'petfood':
-          if (global.db.data.users[m.sender].money >= hpetfood) {
-            global.db.data.users[m.sender].petFood += 1
-            global.db.data.users[m.sender].money -= hpetfood
-            conn.sendMessage(m.chat, { text: `*${htki} شراء ${htka}*\n\nلقد اشتريت *1* طعام حيوانات ب *${hpetfood}* عمله!`, quoted: m })
-          } else {
-            conn.sendMessage(m.chat, { text: `نقودك غير كافية لشراء طعام حيوانات!!`, quoted: m })
-          }
+          conn.sendMessage(m.chat, { text: `*${htki} حيوان أليف جديد !${htka}*\n\n🎉 مبروك، لقد اشتريت حيوان أليف *حصان*`, quoted: m })
           break
         default:
-          conn.sendMessage(m.chat, { text: `*${htki} متجر الحيوانات الأليفة ${htka}*\n\n${caption}` }, { quoted: m })
+          m.reply(`
+*${htki} قائمة الحيوانات الأليفة ${htka}*
+🐈 • *قطة:* ${hcat} رمز الحيوان الأليف🔖
+🐕 • *كلب:* ${hdog} رمز الحيوان الأليف🔖
+🐎 • *حصان:* ${hhorse} رمز الحيوان الأليف🔖
+🦊 • *ثعلب:* ${hfox} رمز الحيوان الأليف🔖
+🍖 • *طعام الحيوان الأليف:* ${hpetfood} نقود 💹
+
+*مثال:*
+${command} قطة
+          `)
           break
       }
+    } else if (/petfood/i.test(command)) {
+      if (user.petfood >= 100)
+        return conn.sendMessage(m.chat, { text: `*💖 لديك بالفعل طعام حيوان أليف بكمية كافية!*`, quoted: m })
+
+      if (user.money < hpetfood)
+        return conn.sendMessage(m.chat, { text: `*💰 رصيدك لا يكفي لشراء طعام الحيوان الأليف!*\n\n💹 يجب أن يكون لديك ${hpetfood} نقود`, quoted: m })
+
+      global.db.data.users[m.sender].money -= hpetfood
+      global.db.data.users[m.sender].petfood += 100
+      conn.sendMessage(m.chat, { text: `*💖 تم شراء طعام حيوان الأليف بنجاح!*\n\n🍖 لقد اشتريت طعام الحيوان الأليف بكمية 100`, quoted: m })
+    } else {
+      m.reply(`
+*${htki} متجر الحيوانات الأليفة ${htka}*
+
+${caption}
+`)
     }
-  } catch (err) {
-    m.reply("Error\n\n\n" + err.stack)
+  } catch (e) {
+    console.log(e)
+    conn.reply(m.chat, `أخطاء!`, m)
   }
 }
-
-handler.help = ['petshop']
-handler.tags = ['ار بي جي']
-handler.command = /^(متجر-الحيوانات)/i
-
-export default handler
+handler.help = ['petshop <قطة|كلب|حصان|ثعلب>']
+handler.tags = ['rpg']
+handler.command = /^(متجر-الحيوانات)$/i
+handler.register = false
+handler.limit = false
+module.exports = handler
