114c8a40dcc7c161b01cb738719e893fa953288a
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 15:19:08 +0300

    Update econ-transfer.js

--- plugins/econ-transfer.js
+++ plugins/econ-transfer.js
@@ -2,44 +2,43 @@ const items = ['diamond', 'exp']
 let confirmation = {}
 
 async function handler(m, { conn, args, usedPrefix, command }) {
-    if (confirmation[m.sender]) return m.reply('You are making a transfer')
+    if (confirmation[m.sender]) return m.reply('أنت في عملية تحويل')
     let user = global.db.data.users[m.sender]
     const item = items.filter(v => v in user && typeof user[v] == 'number')
-    let lol = `✳️ Correct use of the command 
-*${usedPrefix + command}*  [item] [quantity] [@user]
+    let lol = `✳️ الاستخدام الصحيح للأمر
+*${usedPrefix + command}*  [العنصر] [الكمية] [@المستخدم]
 
-📌 Example : 
+📌 مثال :
 *${usedPrefix + command}* exp 65 @${m.sender.split('@')[0]}
 
-📍 transfer items
+📍 تحويل العناصر
 ┌──────────────
-▢ *diamond* = Diamonds 💎
-▢ *exp* = Experience 🆙
+▢ *diamond* = ألماس 💎
+▢ *exp* = XP 🆙
 └──────────────
 `.trim()
     const type = (args[0] || '').toLowerCase()
     if (!item.includes(type)) return conn.reply(m.chat, lol, m, { mentions: [m.sender] })
     const count = Math.min(Number.MAX_SAFE_INTEGER, Math.max(1, (isNumber(args[1]) ? parseInt(args[1]) : 1))) * 1
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[2] ? (args[2].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
-    if (!who) return m.reply('✳️ Tag a user')
-    if (!(who in global.db.data.users)) return m.reply(`✳️ User is not in my database`)
-    if (user[type] * 1 < count) return m.reply(`✳️  *${type}*  insufficient to transfer`)
+    if (!who) return m.reply('✳️ قم بعمل منشن للمستخدم')
+    if (!(who in global.db.data.users)) return m.reply(`✳️ المستخدم غير موجود في قاعدة البيانات`)
+    if (user[type] * 1 < count) return m.reply(`✳️ *${type}* غير كافٍ لإجراء عملية التحويل`)
     let confirm = `
-Are you sure you want to transfer *${count}* _*${type}*_ a  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ? 
+هل أنت متأكد من أنك تريد تحويل *${count}* _*${type}*_ إلى  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ؟ 
 
-- Have  *60s* 
-_respond *yes* o *no*_
+- لديك  *60 ثانية* 
+_استجب بـ *نعم* أو *لا*_
 `.trim()
-   
-    //conn.sendButton(m.chat, confirm, igfg, null, [['yes'], ['no']], m, { mentions: [who] })
-     m.reply(confirm, null, { mentions: [who] })
+
+    m.reply(confirm, null, { mentions: [who] })
     confirmation[m.sender] = {
         sender: m.sender,
         to: who,
         message: m,
         type,
         count,
-        timeout: setTimeout(() => (m.reply('⏳ Time is up'), delete confirmation[m.sender]), 60 * 1000)
+        timeout: setTimeout(() => (m.reply('⏳ انتهى الوقت'), delete confirmation[m.sender]), 60 * 1000)
     }
 }
 
@@ -51,35 +50,29 @@ handler.before = async m => {
     if (m.id === message.id) return
     let user = global.db.data.users[sender]
     let _user = global.db.data.users[to]
-    if (/no?/g.test(m.text.toLowerCase())) {
+    if (/لا?/g.test(m.text.toLowerCase())) {
         clearTimeout(timeout)
         delete confirmation[sender]
-        return m.reply('✅ Transfer cancelled')
+        return m.reply('✅ تم إلغاء عملية التحويل')
     }
-    if (/si?/g.test(m.text.toLowerCase())) {
+    if (/نعم/g.test(m.text.toLowerCase())) {
         let previous = user[type] * 1
         let _previous = _user[type] * 1
         user[type] -= count * 1
         _user[type] += count * 1
-        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ The transfer was made \n\n*${count}* *${type}*  a @${(to || '').replace(/@s\.whatsapp\.net/g, '')}`, null, { mentions: [to] })
+        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ تمت عملية التحويل بنجاح\n\n*${count}* *${type}* إلى  *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
         else {
             user[type] = previous
             _user[type] = _previous
-            m.reply(`❎ Error while transferring *${count}* ${type} a *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
+            m.reply(`❎ حدث خطأ أثناء عملية التحويل *${count}* ${type} إلى *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
         }
         clearTimeout(timeout)
         delete confirmation[sender]
     }
 }
 
-handler.help = ['transfer'].map(v => v + ' [item] [quantity] [@tag]')
-handler.tags = ['econ']
-handler.command = ['payxp','paydi', 'transfer', 'darxp','dardi',]
+handler.help = ['transfer'].map(v => v + ' [العنصر] [الكمية] [@مستخدم]')
+handler.tags = ['اقتصاد']
+handler.command = ['دفعتجربة', 'دفعألماس', 'تحويل', 'دفعنقاط', 'تحويلتجربة', 'تحويلنقاط', 'تحويلألماس']
 
 handler.disabled = false
-
-export default handler
-
-function isNumber(x) {
-    return !isNaN(x)
-}
