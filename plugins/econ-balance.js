1260e3ac075e016687a1a2929bee5f42bcefaf6d
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 13:17:30 +0300

    Update econ-balance.js

--- plugins/econ-balance.js
+++ plugins/econ-balance.js
@@ -1,32 +1,31 @@
-
-let handler = async (m, {conn, usedPrefix}) => {
-	
+let handler = async (m, { conn, usedPrefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+    if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات`
     conn.reply(m.chat, `
-┌───⊷ *BALANCE* ⊶
-▢ *📌Number* : _@${who.split('@')[0]}_
-▢ *💎Diamonds* : _${user.diamond}_
-▢ *💎Gold* : _${user.gold}_
-▢ *💎Rock* : _${user.rock}_
-▢ *💎EMARALD* : _${user.emerald}_
-▢ *💎Rank* : _${user.role}_
-▢ *💎Health* : _${user.health}_
-▢ *💎Wood* : _${user.wood}_
-▢ *💎Potions* : _${user.potion}_
-▢ *💎Iron* : _${user.iron}_
-▢ *💎Money* : _${user.money}_
-▢ *⬆️XP* : _Total ${user.exp}_
+┌───⊷ *الرصيد* ⊶
+▢ *📌الرقم* : _@${who.split('@')[0]}_
+▢ *💎الماس* : _${user.diamond}_
+▢ *💎الذهب* : _${user.gold}_
+▢ *💎الصخور* : _${user.rock}_
+▢ *💎الزمرد* : _${user.emerald}_
+▢ *💎الرتبة* : _${user.role}_
+▢ *💎الصحة* : _${user.health}_
+▢ *💎الخشب* : _${user.wood}_
+▢ *💎الجرعات* : _${user.potion}_
+▢ *💎الحديد* : _${user.iron}_
+▢ *💎المال* : _${user.money}_
+▢ *⬆️نقاط الخبرة* : _الإجمالي ${user.exp}_
 └──────────────
 
-*NOTE :* 
-You can buy 💎 diamonds using the commands*
-❏ *${usedPrefix}todiamond* <amount>
+*ملاحظة:*
+يمكنك شراء 💎 الماس باستخدام الأوامر التالية:
+❏ *${usedPrefix}todiamond* <المبلغ>
 ❏ *${usedPrefix}todiamondall*`, m, { mentions: [who] })
 }
+
 handler.help = ['balance']
-handler.tags = ['econ']
-handler.command = ['bal', 'diamantes', 'diamond', 'balance'] 
+handler.tags = ['اقتصاد']
+handler.command = ['محفظة', 'حقيبة', 'الماس', 'بنك']
 
 export default handler
