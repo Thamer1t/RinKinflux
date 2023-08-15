87884802ff7eb8f13ac6c2ec30e6e6236eba97f6
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-15 12:55:20 +0300

    Update anime-rslap.js

--- plugins/anime-rslap.js
+++ plugins/anime-rslap.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @tag`
+     if (!who) throw `✳️ منشن او رد على شخص\n\n📌 مثال : ${usedPrefix + command} @منشن`
      
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -18,16 +18,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) slapped`, `${name}`)
+   let stiker = await sticker(null, url, `(${name2}) صفع`, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('👊🏻') 
    
 }
 
-handler.help = ['slap @tag']
-handler.tags = ['rnime']
-handler.command = /^(slap|bofetada)$/i
-handler.diamond = true
+handler.help = ['صفع @منشن']
+handler.tags = ['رياكشن']
+handler.command = /^(صفع|slap)$/i
+handler.diamond = false
 handler.group = true
 
 export default handler
